66fae3a44cae87e01324dcaf834f77e1927f5efe
Jae Eun Na <jae-eun.na@linecorp.com>
2021-07-18 00:24:14 +0900

    refactor: rename

--- src/main/java/com/github/lette1394/mediaserver2/core/configuration/infrastructure/MappedResource.java
+++ src/main/java/com/github/lette1394/mediaserver2/core/configuration/infrastructure/MappedResource.java
@@ -1,6 +1,6 @@
 package com.github.lette1394.mediaserver2.core.configuration.infrastructure;
 
 @FunctionalInterface
-interface MappedResource<T> {
+public interface MappedResource<T> {
   T toMapped();
 }
