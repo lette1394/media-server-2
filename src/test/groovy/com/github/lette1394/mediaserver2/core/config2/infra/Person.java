62193fd3a29fb86af395b1d95b1a5f7af247f8df
Jae Eun Na <jae-eun.na@linecorp.com>
2021-08-01 03:26:39 +0900

    refactor

--- src/test/groovy/com/github/lette1394/mediaserver2/core/config2/infra/Person.java
+++ src/test/groovy/com/github/lette1394/mediaserver2/core/config2/infra/Person.java
@@ -1,12 +1,11 @@
 package com.github.lette1394.mediaserver2.core.config2.infra;
 
 import lombok.Builder;
-import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Jacksonized
-public class Person {
+class Person {
   private final String name;
   private final int age;
 }
