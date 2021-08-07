3f95914cbede1f66e8948b2dc2607e3bc6f808ae
Jae Eun Na <jae-eun.na@linecorp.com>
2021-08-08 03:06:10 +0900

    refactor: config

--- src/main/java/com/github/lette1394/mediaserver2/core/config2/infra/Annotation.java
+++ src/main/java/com/github/lette1394/mediaserver2/core/config2/infra/Annotation.java
@@ -8,50 +8,48 @@ import com.github.lette1394.mediaserver2.core.config2.domain.Config;
 import com.github.lette1394.mediaserver2.core.config2.domain.ConfigException;
 import com.github.lette1394.mediaserver2.core.config2.domain.ConfigLocation;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 class Annotation implements FileKeyFactory {
   @Override
   public <T> FileKey<T> create(Class<T> configType) {
-    final var configAnnotation = configType.getDeclaredAnnotation(Config.class);
-    requires(configAnnotation != null, () -> new ConfigException("""
-      config class should have @Config annotation"""));
-
-    final var configLocations = configLocations(configType);
-    requires(configLocations.size() == 1, () -> new ConfigException("""
+    requireConfig(configType);
+    requires(configLocations(configType).size() == 1, () -> new ConfigException("""
       config class should have at exactly one @ConfigLocation annotation"""));
 
-    final var fileKeys = configLocations
-      .stream()
-      .map(ConfigLocation::value)
-      .flatMap(location -> FileKey.scan(configType, location).stream())
-      .collect(toUnmodifiableSet());
-    requires(fileKeys.size() == 1);
+    final var nameKeyMap = toNameKeyMap(configType);
+    requires(nameKeyMap.size() == 1);
 
-    return fileKeys.iterator().next();
+    return nameKeyMap.values().iterator().next();
   }
 
   @Override
   public <T> FileKey<T> create(Class<T> configType, String name) {
-    final var configAnnotation = configType.getDeclaredAnnotation(Config.class);
-    requires(configAnnotation != null, () -> new ConfigException("""
-      config class should have @Config annotation"""));
-
-    final var configLocations = configLocations(configType);
-    requires(configLocations.size() > 0, () -> new ConfigException("""
+    requireConfig(configType);
+    requires(configLocations(configType).size() > 0, () -> new ConfigException("""
       config class should have at least one @ConfigLocation annotation"""));
 
-    final var nameFileKeyMap = configLocations
+    final var nameFileKeyMap = toNameKeyMap(configType);
+    requires(nameFileKeyMap.containsKey(name), () -> new ConfigException("unknown config name: [%s]".formatted(name)));
+
+    return nameFileKeyMap.get(name);
+  }
+
+  private <T> Map<String, FileKey<T>> toNameKeyMap(Class<T> configType) {
+    return configLocations(configType)
       .stream()
       .map(ConfigLocation::value)
       .flatMap(location -> FileKey.scan(configType, location).stream())
       .collect(toMap(fileKey -> fileKey.path().toFile().getName(), Function.identity()));
-    requires(nameFileKeyMap.containsKey(name), () -> new ConfigException("unknown config name: [%s]".formatted(name)));
+  }
 
-    return nameFileKeyMap.get(name);
+  private <T> void requireConfig(Class<T> configType) {
+    final var configAnnotation = configType.getDeclaredAnnotation(Config.class);
+    requires(configAnnotation != null, () -> new ConfigException("""
+      config class should have @Config annotation"""));
   }
 
   private static List<ConfigLocation> configLocations(Class<?> configType) {
